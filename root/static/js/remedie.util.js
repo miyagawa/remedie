f5ea9d347a423ee64134fbe901e906ff0d6b0b9d
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-11-02 08:18:49 +0000

    oops, add

---
+++ root/static/js/remedie.util.js
@@ -0,0 +1,27 @@
+function RemedieUtil() { }
+RemedieUtil.prototype = { };
+RemedieUtil.calcWindowSize = function(window_width) {
+  var widths = [ 1920, 1280, 1024, 704 ];
+  var i = 0;
+  while (window_width < widths[i]) {
+    i++;
+    if (!widths[i]) {
+      i--; break;
+    }
+  }
+  return [ widths[i], widths[i] * 9/16 ];
+};
+
+RemedieUtil.formatBytes = function(bytes) {
+  if (bytes == 0) return '(Unknown)';
+  var units = [ 'bytes', 'KB', 'MB', 'GB', 'TB' ];
+  var i = 0;
+  while (bytes > 1024) {
+    bytes = bytes / 1024;
+    i++;
+    if (!units[i]) break;
+  }
+
+  return $.sprintf('%.1f %s', bytes, units[i]);    
+};
+
