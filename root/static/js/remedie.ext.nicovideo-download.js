20edc56ca5abcb8a8dff53da3d4651f22adf0ba4
mattn <mattn.jp@gmail.com>
2008-11-20 08:49:45 +0000

    serv cgi action.

--- root/static/js/remedie.ext.nicovideo-download.js
+++ root/static/js/remedie.ext.nicovideo-download.js
@@ -1,8 +1,7 @@
 if (typeof remedie != 'undefined') {
     remedie.addAction(/nicovideo/, 'Download Video', function(item) {
         if (/.*(sm[0-9]+)$/.test(item.ident)) {
-            // TODO better to run on remedie's http server. 
-            window.open("http://localhost:8080/nph-download-proxy.cgi/" + RegExp.$1);
+            window.open("/action/nph-download-proxy.cgi/" + RegExp.$1);
         }
     });
 }
