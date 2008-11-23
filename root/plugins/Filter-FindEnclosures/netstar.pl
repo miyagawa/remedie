354c4b788f5010aedb3606400c1da8fad73f5495
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-11-23 10:28:50 +0000

    remove debugging

--- root/plugins/Filter-FindEnclosures/netstar.pl
+++ root/plugins/Filter-FindEnclosures/netstar.pl
@@ -10,7 +10,6 @@ sub needs_content { 0 }
 sub find {
     my ($self, $args) = @_;
 
-    warn $args->{url};
     $args->{url} =~ qr!http://www.nhk-ep.co.jp/netstar/(\w+)_mov_(\w+\d+)\.html!
         or return;
     my($name, $id) = ($1, $2);
