78c44857cab2aa71c3532440a8bb171ecb689699
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-11-23 22:45:24 +0000

    netstar.pl junky hack to get thumbnail

--- root/plugins/Filter-FindEnclosures/netstar.pl
+++ root/plugins/Filter-FindEnclosures/netstar.pl
@@ -13,9 +13,12 @@ sub find {
     $args->{content} =~ m!<embed src="(.*?)\.swf"!i
         or return;
 
+    my $id = $1;
+    (my $thumb_id = $id) =~ s/sono(\d+)/s$1/;
+
     my $enclosure = Plagger::Enclosure->new;
-    $enclosure->url("http://www.nhk-ep.co.jp/netstar/flv/$1.flv");
+    $enclosure->url("http://www.nhk-ep.co.jp/netstar/flv/${id}.flv");
     $enclosure->type("video/x-flv");
-    $enclosure->thumbnail({ url => "http://www.nhk-ep.co.jp/netstar/img/mov_thumb_$1.jpg" });
+    $enclosure->thumbnail({ url => "http://www.nhk-ep.co.jp/netstar/img/mov_thumb_${thumb_id}.jpg" });
     return $enclosure;
 }
