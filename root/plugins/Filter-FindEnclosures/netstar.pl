bb86e08e8d505a3fdc96b93829b43d34fa6753a6
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-11-23 22:41:53 +0000

    update netstar plugin

--- root/plugins/Filter-FindEnclosures/netstar.pl
+++ root/plugins/Filter-FindEnclosures/netstar.pl
@@ -2,21 +2,20 @@
 sub init {
     my $self = shift;
     $self->{domain} = "www.nhk-ep.co.jp";
-    $self->{handle} = "/netstar/.*_mov_";
+    $self->{handle} = "/netstar/.*mov_";
 }
 
-sub needs_content { 0 }
+sub needs_content { 1 }
 
 sub find {
     my ($self, $args) = @_;
 
-    $args->{url} =~ qr!http://www.nhk-ep.co.jp/netstar/(\w+)_mov_(\w+\d+)\.html!
+    $args->{content} =~ m!<embed src="(.*?)\.swf"!i
         or return;
-    my($name, $id) = ($1, $2);
 
     my $enclosure = Plagger::Enclosure->new;
-    $enclosure->url("http://www.nhk-ep.co.jp/netstar/flv/${name}_$id.flv");
+    $enclosure->url("http://www.nhk-ep.co.jp/netstar/flv/$1.flv");
     $enclosure->type("video/x-flv");
-    $enclosure->thumbnail({ url => "http://www.nhk-ep.co.jp/netstar/img/mov_thumb_${name}_${id}.jpg" });
+    $enclosure->thumbnail({ url => "http://www.nhk-ep.co.jp/netstar/img/mov_thumb_$1.jpg" });
     return $enclosure;
 }
