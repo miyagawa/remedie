62bb03f07fdbb3ab048e17983eaa61bd29f38b1d
mattn <mattn.jp@gmail.com>
2008-11-26 10:20:26 +0000

    use "<:raw"

--- lib/Remedie/Server.pm
+++ lib/Remedie/Server.pm
@@ -158,8 +158,7 @@ sub serve_static_file {
             }
         }
 
-        open my $fh, "<", $file or die "$file: $!";
-        binmode $fh;
+        open my $fh, "<:raw", $file or die "$file: $!";
         $res->headers->header('Last-Modified' => HTTP::Date::time2str($mtime));
         $res->headers->header('Content-Length' => $size);
         $res->body( join '', <$fh> );
