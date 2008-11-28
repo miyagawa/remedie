c18e5f3047e2bee4cd4699fdc1790d243b4ce8b6
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-11-28 17:02:29 +0000

    Added thumbnail for pandora.tv

--- root/plugins/Filter-FindEnclosures/pandoratv.pl
+++ root/plugins/Filter-FindEnclosures/pandoratv.pl
@@ -21,6 +21,25 @@ sub find {
     $enclosure->url($data->{flv});
     $enclosure->type('video/x-flv');
     $enclosure->length($data->{filesize});
-    # TODO use $data->{isHD}
+    $enclosure->thumbnail({ url => $self->thumbnail_for($args->{url}) });
+    if ($data->{isHD}) {
+        $enclosure->width(1280);
+        $enclosure->height(720);
+    }
+
     return $enclosure;
+}
+
+sub thumbnail_for {
+    my($self, $url) = @_;
+
+    # URL http://channel.pandora.tv/channel/video.ptv?ch_userid=dkxtr291&skey=Perfume&prgid=33740367&categid=all&page=1
+    # thumb http://imguser.pandora.tv/pandora/_channel_img_sm/d/k/dkxtr291/67/vod_thumb_33740367.jpg
+
+    my $uri = URI->new($url);
+    my $user  = $uri->query_param('ch_userid');
+    my $prgid = $uri->query_param('prgid');
+
+    return sprintf "http://imguser.pandora.tv/pandora/_channel_img_sm/%s/%s/%s/%s/vod_thumb_%s.jpg",
+        substr($user, 0, 1), substr($user, 1, 1), $user, substr($prgid, -2, 2), $prgid;
 }
