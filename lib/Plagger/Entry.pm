f7ff121faf3ea69015b2f69efd7f4da2bb171b60
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-12-13 08:31:24 +0000

    prefer enclosure thumbnail to workaround youtube spacer pixel gif found in FindLinks

--- lib/Plagger/Entry.pm
+++ lib/Plagger/Entry.pm
@@ -82,7 +82,7 @@ sub add_enclosure {
     # don't add enclosure with the same URL again and again
     unless ($enclosure->url && grep { $_->url && $_->url eq $enclosure->url } $self->enclosures) {
         Plagger->context->run_hook('enclosure.add', { entry => $self, enclosure => $enclosure });
-        if ($enclosure->thumbnail && !$self->icon) {
+        if ($enclosure->thumbnail) {
             $self->icon($enclosure->thumbnail);
         }
         push @{ $self->{enclosures} }, $enclosure;
