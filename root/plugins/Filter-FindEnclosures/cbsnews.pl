e562ceb6dd9c82882e867a2c543f95778b3a8418
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-12-13 04:50:53 +0000

    Added width/height to CBS News

--- root/plugins/Filter-FindEnclosures/cbsnews.pl
+++ root/plugins/Filter-FindEnclosures/cbsnews.pl
@@ -17,5 +17,7 @@ sub find {
     my $enclosure = Plagger::Enclosure->new;
     $enclosure->url("http://www.cbs.com/thunder/swf30can10cbsnews/rcpHolderCbs-3-4x3.swf?releaseURL=http://release.theplatform.com/content.select?pid=$pid");
     $enclosure->type('application/x-shockwave-flash');
+    $enclosure->width(425);
+    $enclosure->height(324);
     return $enclosure;
 }
