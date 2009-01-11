8b30b891685f01e17fcd2ee319ab2d4d6c2880d1
Masafumi Otsune <info@otsune.com>
2009-01-11 21:34:51 +0800

    Add scraper for so-net rakugo
    Signed-off-by: Tatsuhiko Miyagawa <miyagawa@gmail.com>

---
+++ root/plugins/so-net.ne.jp/scraper.pl
@@ -0,0 +1,19 @@
+# http://www.so-net.ne.jp/minnanotv/rakugo/rakugokai.html
+sub init {
+    my $self = shift;
+    $self->{handle} = "/minnanotv/rakugo/";
+}
+
+sub build_scraper {
+    scraper {
+        process  '//tr[td[./table[.//tr[td[a[contains(@href, ".wvx")]]] and .//span]]]', 'entries[]' => scraper {
+            process 'span.title', title => 'TEXT';
+            process 'span.honbun', body => 'TEXT';
+            process '//a[contains(@href,".wvx")]', enclosure => [ '@href',
+            sub { +{ url => $_, type => 'video/x-ms-wvx' } } ];
+            process '//img[contains(@src, ".jpg")]', thumbnail => [ '@src', sub { +{ url => $_ } } ];
+            process '//a[@target="_blank"]', link => '@href',
+        };
+        process 'title', title => 'TEXT';
+    };
+}
