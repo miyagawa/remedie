551320636687c0da1e1bfc4a5b2da53b7ddae1fe
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-01-12 17:05:12 -0800

    fix the way assets_dir is escaped

--- lib/Plagger/Plugin.pm
+++ lib/Plagger/Plugin.pm
@@ -91,8 +91,8 @@ sub load_assets {
     );
 
     # $rule isa File::Find::Rule
-    my $assets_dir = qr|\Q$self->assets_dir\E|;
-    for my $file ($rule->in($assets_dir)) {
+    my $assets_dir = quotemeta $self->assets_dir;
+    for my $file ($rule->in($self->assets_dir)) {
         my $domain = File::Basename::dirname($file);
         $domain =~ s/^$assets_dir\///;
         $domain = '*' if $domain eq 'default';
