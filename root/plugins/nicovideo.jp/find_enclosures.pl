7dd5fcd787a69a947576bf3044f66b034b61d8e8
Kota Saito <kotas.nico@gmail.com>
2009-01-14 20:16:40 +0900

    use thumbnail image in entry body for nicovideo.jp

--- root/plugins/nicovideo.jp/find_enclosures.pl
+++ root/plugins/nicovideo.jp/find_enclosures.pl
@@ -1,4 +1,6 @@
 # author: Tatsuhiko Miyagawa
+use HTML::TreeBuilder::XPath;
+
 sub init {
     my $self = shift;
     $self->{handle} = "/watch/";
@@ -8,32 +10,20 @@ sub needs_content { 0 }
 
 sub find {
     my ($self, $args) = @_;
-    my($id) = $args->{url} =~ qr!watch/(\w\w\d+)!
+    my($id) = $args->{url} =~ qr!watch/(\w\w[^/?]+|\d+)!
         or return;
+
+    my $thumbnail = $self->_thumbnail_for($args->{entry});
 
     my $enclosure = Plagger::Enclosure->new;
     $enclosure->url("http://ext.nicovideo.jp/thumb_watch/$id");
     $enclosure->type('application/javascript');
-    $enclosure->thumbnail({ url => $self->_thumbnail_for($id) });
+    $enclosure->thumbnail({ url => $thumbnail }) if $thumbnail;
     return $enclosure;
 }
 
 sub _thumbnail_for {
-    my($self, $id) = @_;
-
-    if (0) {
-    my $res = Plagger::UserAgent->new->fetch("http://www.nicovideo.jp/api/getthumbinfo/$id");
-
-    unless ($res->is_error) {
-        my $doc = XML::LibXML->new->parse_string($res->content);
-
-        my @nodes = $doc->findnodes('//thumbnail_url');
-        if (@nodes) {
-            return $nodes[0]->textContent;
-        }
-    }
-    }
-
-    $id =~ s!^\w\w!!;
-    return "http://tn-skr1.smilevideo.jp/smile?i=$id";
+    my($self, $entry) = @_;
+    my $tree = HTML::TreeBuilder::XPath->new_from_content($entry->body);
+    return $tree->findvalue('//p[@class="nico-thumbnail"]/img/@src');
 }
