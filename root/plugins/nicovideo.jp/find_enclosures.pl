1669a1c61d7cb9c912834bd51c266f3c5992f36a
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-01-14 11:23:25 -0800

    fallback to the old ad-hoc URL builder if the entry doesn't have thumbnail in its body (e.g. mitter feeds)

--- root/plugins/nicovideo.jp/find_enclosures.pl
+++ root/plugins/nicovideo.jp/find_enclosures.pl
@@ -1,6 +1,4 @@
 # author: Tatsuhiko Miyagawa
-use HTML::TreeBuilder::XPath;
-
 sub init {
     my $self = shift;
     $self->{handle} = "/watch/";
@@ -13,7 +11,7 @@ sub find {
     my($id) = $args->{url} =~ qr!watch/(\w\w[^/?]+|\d+)!
         or return;
 
-    my $thumbnail = $self->_thumbnail_for($args->{entry});
+    my $thumbnail = $self->_thumbnail_for($id, $args->{entry});
 
     my $enclosure = Plagger::Enclosure->new;
     $enclosure->url("http://ext.nicovideo.jp/thumb_watch/$id");
@@ -23,7 +21,17 @@ sub find {
 }
 
 sub _thumbnail_for {
-    my($self, $entry) = @_;
-    my $tree = HTML::TreeBuilder::XPath->new_from_content($entry->body);
-    return $tree->findvalue('//p[@class="nico-thumbnail"]/img/@src');
+    my($self, $id, $entry) = @_;
+
+    # Extract thumbnails if the source feed is on nicovideo.jp
+    # otherwise, generate the thumbnail using the ad-hoc URL construction
+    if ($entry->body =~ /nico-thumbnail/) {
+        require HTML::TreeBuilder::XPath;
+        my $tree = HTML::TreeBuilder::XPath->new_from_content($entry->body);
+        return $tree->findvalue('//p[@class="nico-thumbnail"]/img/@src');
+    } elsif ($id =~ s/^\w\w//) {
+        return "http://tn-skr1.smilevideo.jp/smile?i=$id";
+    }
+
+    return;
 }
