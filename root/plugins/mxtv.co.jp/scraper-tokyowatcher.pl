a26d80bdbd4d78cc55405621a8f4d9409601764d
MATSUU Takuto <matsuu@gentoo.org>
2009-01-16 08:45:22 +0900

    Add scraper TOKYO MX NEWS TOKYO Watcher

---
+++ root/plugins/mxtv.co.jp/scraper-tokyowatcher.pl
@@ -0,0 +1,20 @@
+# http://www.mxtv.co.jp/mxnews/mx_free/tokyo/
+sub init {
+    my $self = shift;
+    $self->{handle} = "/mxnews/mx_free/tokyo/";
+}
+
+sub build_scraper {
+    scraper {
+        process  '//table[@class="movie_table"]', 'entries[]' => scraper {
+            process '//td[@class="movie_title"]//a', title => 'TEXT';
+            process '//td[@class="movie_date"]', date => 'TEXT';
+            process '//td[@class="movie_title"]//a[contains(@href,".asx")]', enclosure => [ '@href',
+            sub { +{ url => $_, type => 'video/x-ms-asf' } } ];
+            process '//td[@class="movie_pic"]//img', thumbnail => [ '@src',
+            sub { +{ url => $_ } } ];
+        };
+        result->{title} = 'TOKYO MX ＊ TOKYO MX NEWS 「TOKYO Watcher」';
+        result;
+    };
+}
