ac9f3990d95063b980cdb5b60b6b5e833e35ba57
mattn <mattn.jp@gmail.com>
2009-01-15 09:46:27 +0900

    add screentoaster.com

---
+++ root/plugins/screentoaster.com/find_enclosures.pl
@@ -0,0 +1,19 @@
+# author: Yasuhiro Matsumoto
+
+sub init {
+    my $self = shift;
+    $self->{handle} = ".";
+}
+
+sub needs_content { 0 }
+
+sub find {
+    my ($self, $args) = @_;
+    my $uri = URI->new($args->{url});
+    my $video_id = ($uri =~ m!/watch/([\w\-]+)!)[0] or return;
+
+    my $enclosure = Plagger::Enclosure->new;
+    $enclosure->url("http://www.screentoaster.com/swf/STPlayer.swf?video=$video_id");
+    $enclosure->type('application/x-shockwave-flash');
+    return $enclosure;
+}
