9953d7b444b1d6aacd93ea3f26d79d8ecfa2fd4a
Masafumi Otsune <info@otsune.com>
2009-01-15 13:22:49 +0800

    some fix (thx mattn)
    Signed-off-by: Tatsuhiko Miyagawa <miyagawa@gmail.com>

--- root/plugins/animate.tv/scraper-szbh.pl
+++ root/plugins/animate.tv/scraper-szbh.pl
@@ -1,7 +1,7 @@
 # http://www.animate.tv/digital/web_radio/detail_104.html
 sub init {
     my $self = shift;
-    $self->{handle} = "/digital/web_radio/detail_104.html";
+    $self->{handle} = "/digital/web_radio/";
 }
 
 sub build_scraper {
@@ -11,11 +11,13 @@ sub build_scraper {
             process '//table[@width="553"]//td[@class="main_txt1"]', body => 'TEXT';
             process 'td.main_txt2', date => 'TEXT';
             process '//a[contains(@href, ".asx")]', enclosure => [ '@href',
-            sub { +{ url => $_, type => 'video/x-ms-asf' } } ];
+                    sub { +{ url => $_, type => 'video/x-ms-asf' } } ];
             process '//a[contains(@href, ".asx")]', link => '@href';
         };
         process 'title', title => 'TEXT';
-        process 'span.main_title3 > strong > img', image => ['@src', sub { +{ url => $_} } ];
-        process '//td[@class="main_txt1" and descendant::a[contains(@href,"zetsubou_mail.html")] ]', 'description' => 'TEXT';
+        process '//*[@class="main_title3"]//img[contains(@src, "logo")]',
+                image => ['@src', sub { +{ url => $_} } ];
+        process '//table[@width="573"]//td[@class="main_txt1"]',
+                description => 'TEXT';
     };
 }
