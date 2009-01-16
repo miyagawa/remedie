04834c0931dfff5fe33a59693d54cb997cbcd095
Masafumi Otsune <info@otsune.com>
2009-01-16 14:23:00 +0900

    add date filter
    Signed-off-by: Tatsuhiko Miyagawa <miyagawa@bulknews.net>

--- root/plugins/animate.tv/scraper-webradio.pl
+++ root/plugins/animate.tv/scraper-webradio.pl
@@ -9,7 +9,8 @@ sub build_scraper {
         process '//table[@width="565" and descendant::a[contains(@href,".asx")] ]', 'entries[]' => scraper {
             process 'td.main_title2', title => 'TEXT';
             process '//table[@width="553"]//td[@class="main_txt1"]', body => 'TEXT';
-            process 'td.main_txt2', date => 'TEXT';
+            process 'td.main_txt2', date => ['TEXT',
+                    sub { s/.*?(\d{4})\x{5e74}(\d{1,2})\x{6708}(\d{1,2})\x{65e5}.*/$1-$2-$3/ } ];
             process '//a[contains(@href, ".asx")]', enclosure => [ '@href',
                     sub { +{ url => $_, type => 'video/x-ms-asf' } } ];
             process '//a[contains(@href, ".asx")]', link => '@href';
