bbcca3f5bda14b98c4f7b96be89f3b8a4d772de3
mattn <mattn.jp@gmail.com>
2009-02-05 14:01:57 +0900

    added NOTE "specify your CGI script".

--- root/static/js/remedie.ext.nicovideo-download.js
+++ root/static/js/remedie.ext.nicovideo-download.js
@@ -1,7 +1,8 @@
 if (typeof remedie != 'undefined') {
     remedie.addAction(/nicovideo/, 'Download file via CGI', function(item) {
         if (/.*(sm[0-9]+)$/.test(item.ident)) {
-            window.open("/action/nph-download-proxy.cgi/" + RegExp.$1);
+            // NOTE: specify your script. you can get this script from WWW::NicoVideo::Download
+            window.open("http://127.0.0.1/cgi-bin/nph-download-proxy.cgi/" + RegExp.$1);
         }
     });
 }
