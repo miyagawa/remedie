miyagawa/remedie

commit 77c411837fb9b6af2e9034379d4d208cd46023f1

fix tests and mostly undefined warnings

lib/Plagger/Plugin/Aggregator/Simple.pm
@@ -35,14 +35,12 @@ sub aggregate {
     my($self, $context, $args) = @_;
 
     my $res = $args->{feed}->source or return;
-    my $feed_url = Plagger::FeedParser->discover($res);
+    my $feed_url = Plagger::FeedParser->discover($res) or return;
     if ($res->uri eq $feed_url) {
         return $self->handle_feed($feed_url, \$res->content, $args->{feed});
-    } elsif ($feed_url && !$self->conf->{no_discovery}) {
+    } elsif (!$self->conf->{no_discovery}) {
         $res = $self->fetch_content($feed_url) or return;
         return $self->handle_feed($feed_url, \$res->content, $args->{feed});
-    } else {
-        return;
     }
 
     return 1;
@@ -204,7 +202,8 @@ sub feed_to_text {
         # in Atom, be a little strict with TextConstruct
         # TODO: this actually doesn't work since XML::Feed and XML::Atom does the right
         # thing with Atom 1.0 TextConstruct
-        if ($content->type eq 'text/plain' || $content->type eq 'text') {
+        my $content_type = $content->type || '';
+        if ($content_type && ($content_type eq 'text/plain' || $content_type eq 'text')) {
             return Plagger::Text->new(type => 'text', data => $content->body);
         } else {
             return Plagger::Text->new(type => 'html', data => $content->body);

Makefile.PL
@@ -71,6 +71,7 @@ requires 'YAML::Syck';
 build_requires 'Test::More';
 use_test_base;
 auto_include_deps;
+tests 't/plagger/core/*.t t/plagger/plugins/*/*.t';
 author_tests('xt');
 WriteAll;
 

extlib/Filesys/Virtual/Plain.pm
@@ -194,7 +194,7 @@ sub root_path {
 	my ($self) = shift;
 
 	if (@_) {
-		my $root_path = shift;
+		my $root_path = shift || '';
 			
 		### Does the root path end with a '/'?  If so, remove it.
 		$root_path = (substr($root_path, length($root_path)-1, 1) eq '/') ?

lib/Plagger.pm
@@ -217,6 +217,7 @@ sub run_hook {
     my @ret;
     for my $action (@{ $self->{hooks}->{$hook} }) {
         my $plugin = $action->{plugin};
+        no warnings 'redefine';
         local *Plagger::current_plugin = sub { $plugin };
         my $ret = $action->{callback}->($plugin, $self, $args);
         $callback->($ret) if $callback;

lib/Plagger/Enclosure.pm
@@ -30,12 +30,12 @@ sub auto_set_type {
 
 sub media_type {
     my $self = shift;
-    ( split '/', $self->type )[0] || 'unknown';
+    ( split '/', ($self->type || '') )[0] || 'unknown';
 }
 
 sub sub_type {
     my $self = shift;
-    ( split '/', $self->type )[1] || 'unknown';
+    ( split '/', ($self->type || '') )[1] || 'unknown';
 }
 
 sub filename {

lib/Plagger/Plugin/Filter/FindEnclosures.pm
@@ -97,9 +97,9 @@ sub add_enclosure_from_head {
     my(%meta, %link);
     while (my $tag = $parser->get_tag('meta', 'link', '/head')) {
         last if $tag->[0] eq '/head';
-        if ($tag->[0] eq 'meta') {
+        if ($tag->[0] eq 'meta' && $tag->[1]{name}) {
             $meta{$tag->[1]{name}} = $tag->[1]{content};
-        } elsif ($tag->[0] eq 'link') {
+        } elsif ($tag->[0] eq 'link' && $tag->[1]{rel}) {
             $link{$tag->[1]{rel}} = $tag->[1]{href};
         }
     }

t/plagger/plugins/CustomFeed-Filesys/base.t
@@ -1,5 +1,6 @@
 use strict;
 use t::TestPlagger;
+use lib "extlib";
 
 test_plugin_deps;
 plan 'no_plan';

t/plagger/plugins/Discovery-Sites/base.t
@@ -30,10 +30,11 @@ plugins:
       feed:
         - http://www.veoh.com/search.html?type=v&search=obama
   - module: Discovery::Sites
+  - module: CustomFeed::FindLinks
 
 --- expected
-like $context->subscription->feeds->[0]->entries->[0]->link, qr|http://www.veoh.com/videos|;
-ok $context->subscription->feeds->[0]->entries->[0]->thumbnail->{url};
+like $context->update->feeds->[0]->entries->[0]->link, qr|http://www.veoh.com/browse/videos|;
+ok $context->update->feeds->[0]->entries->[0]->thumbnail->{url};
 
 ===
 --- input config
@@ -45,5 +46,5 @@ plugins:
   - module: Discovery::Sites
 
 --- expected
-like $context->subscription->feeds->[0]->entries->[0]->enclosures->[0]->url, qr/720p.m[04]v/;
-like $context->subscription->feeds->[0]->entries->[0]->date, qr/20/;
+like $context->update->feeds->[0]->entries->[0]->enclosures->[0]->url, qr/720p.m[04]v/;
+like $context->update->feeds->[0]->entries->[0]->date, qr/20/;

commit 0d01f2dabf4353b60f25e4ea3df13cf52e40fb61

77c41183 broke customFeeds

lib/Plagger/Plugin/Aggregator/Simple.pm
@@ -41,9 +41,9 @@ sub aggregate {
     } elsif (!$self->conf->{no_discovery}) {
         $res = $self->fetch_content($feed_url) or return;
         return $self->handle_feed($feed_url, \$res->content, $args->{feed});
+    } else {
+        return;
     }
-
-    return 1;
 }
 
 sub fetch_content {