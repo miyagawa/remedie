17cbee3478c5a25cb4ffbfe3c4f935afdcae02d0
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-02-08 23:08:41 -0800

    make this 0.3.1

--- lib/Remedie.pm
+++ lib/Remedie.pm
@@ -2,7 +2,7 @@ package Remedie;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.3.0';
+our $VERSION = '0.3.1';
 
 1;
 __END__
--- root/static/js/remedie.js
+++ root/static/js/remedie.js
@@ -1,4 +1,4 @@
-Remedie.version = '0.3.0';
+Remedie.version = '0.3.1';
 
 function Remedie() {
   this.initialize();
