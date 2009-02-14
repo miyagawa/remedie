132f002c55ba4732892e3d9443878c003b457362
MATSUU Takuto <matsuu@gentoo.org>
2009-02-15 02:07:54 +0800

    add find enclosures for slideshare.net
    Signed-off-by: Tatsuhiko Miyagawa <miyagawa@gmail.com>

---
+++ root/plugins/slideshare.net/find_enclosures.pl
@@ -0,0 +1,21 @@
+use URI;
+sub init {
+    my $self = shift;
+    $self->{handle} = ".";
+}
+
+sub needs_content { 1 }
+
+sub find {
+    my ($self, $args) = @_;
+
+    my ($url) = $args->{content} =~ m!<param name="movie" value="([^"]*)"!
+        or return;
+
+    warn $url;
+
+    my $enclosure = Plagger::Enclosure->new;
+    $enclosure->url(URI->new($url));
+    $enclosure->type('application/x-shockwave-flash');
+    return $enclosure;
+}
