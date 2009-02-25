ddb9e844901f1ef953e706cefccd02c85448cbd9
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-02-25 13:08:46 -0800

    Added dpj.or.jp media press conference

---
+++ root/plugins/dpj.or.jp/find_enclosures.pl
@@ -0,0 +1,22 @@
+use Web::Scraper;
+sub init {
+    my $self = shift;
+    $self->{handle} = '/news/\?num=\d+';
+}
+
+sub needs_content { 1 }
+
+# Just a standard .asx link, but make sure to pick up the right link
+sub find {
+    my($self, $args) = @_;
+
+    my $res = scraper {
+        process '//a[contains(@href, "v300.asx")', link => '@href';
+    }->scrape($args->{content});
+
+    my $url = $res->{link} or return;
+    my $enclosure = Plagger::Enclosure->new;
+    $enclosure->url($url);
+
+    return $enclosure;
+}
