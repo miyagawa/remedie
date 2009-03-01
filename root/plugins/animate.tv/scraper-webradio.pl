ef304558fdf8722b3867cda1640ce56e9d27d383
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-03-01 03:38:06 -0800

    animate.tv: get thumbnail for the latest episode

--- root/plugins/animate.tv/scraper-webradio.pl
+++ root/plugins/animate.tv/scraper-webradio.pl
@@ -6,6 +6,10 @@ sub init {
 
 sub build_scraper {
     scraper {
+        my($thumb, $idx);
+        process '//td[@width="240" and @align="center"]/img[@width="240" and @height="180"]',
+            _image => [ '@src', sub { $thumb = $_ } ];
+
         process '//table[@width="565" and descendant::a[contains(@href,".asx")] ]', 'entries[]' => scraper {
             process 'td.main_title2', title => 'TEXT';
             process '//table[@width="553"]//td[@class="main_txt1"]', body => 'TEXT';
@@ -14,6 +18,10 @@ sub build_scraper {
             process '//a[contains(@href, ".asx")]', enclosure => [ '@href',
                     sub { +{ url => $_, type => 'video/x-ms-asf' } } ];
             process '//a[contains(@href, ".asx")]', link => '@href';
+            process 'a', thumbnail => sub {
+                return unless !$idx++ && $thumb;
+                $thumb;
+            };
         };
         process 'title', title => 'TEXT';
         process '//*[@class="main_title3"]//img[contains(@src, "logo")]',
