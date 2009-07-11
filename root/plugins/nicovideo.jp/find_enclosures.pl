54070546b59951ed7a915c159083425c3b2273b6
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-07-11 02:38:02 -0700

    one last HTML::TreeBuilder leak

--- root/plugins/nicovideo.jp/find_enclosures.pl
+++ root/plugins/nicovideo.jp/find_enclosures.pl
@@ -1,9 +1,10 @@
 # author: Tatsuhiko Miyagawa
+use HTML::TreeBuilder::LibXML;
+
 sub init {
     my $self = shift;
     $self->{handle} = "/watch/";
 }
-
 
 sub find {
     my ($self, $args) = @_;
@@ -25,9 +26,11 @@ sub _thumbnail_for {
     # Extract thumbnails if the source feed is on nicovideo.jp
     # otherwise, generate the thumbnail using the ad-hoc URL construction
     if ($entry->body =~ /nico-thumbnail/) {
-        require HTML::TreeBuilder::XPath;
-        my $tree = HTML::TreeBuilder::XPath->new_from_content($entry->body);
-        return $tree->findvalue('//p[@class="nico-thumbnail"]/img/@src');
+        my $tree = HTML::TreeBuilder::LibXML->new;
+        $tree->parse($entry->body);
+        my @tags = $tree->findvalue('//p[@class="nico-thumbnail"]/img/@src');
+        $tree->delete;
+        return @tags;
     } elsif ($id =~ s/^\w\w//) {
         return "http://tn-skr1.smilevideo.jp/smile?i=$id";
     }
