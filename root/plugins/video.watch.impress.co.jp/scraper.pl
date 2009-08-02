3d50f95bcf4e1f3f68875b1ca5aff0d7738fce10
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-08-02 13:30:11 -0700

    fix CSS selector

--- root/plugins/video.watch.impress.co.jp/scraper.pl
+++ root/plugins/video.watch.impress.co.jp/scraper.pl
@@ -2,7 +2,7 @@ sub init { }
 
 sub build_scraper {
     scraper {
-        process ".backnoItem", "entries[]" => scraper {
+        process ".ipw_contents", "entries[]" => scraper {
             process '//div[contains(@class, "title_")]', title => 'TEXT';
             process ".subtitle", body => 'TEXT';
             process "img.top_mainimg", thumbnail => '@src';
