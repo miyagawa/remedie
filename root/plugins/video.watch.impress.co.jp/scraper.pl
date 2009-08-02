5d8124b20af1a2e61a4e89fc416e812eaa2e5db3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-08-02 13:20:19 -0700

    fixed impress watch scraper for non-stapa video channel

--- root/plugins/video.watch.impress.co.jp/scraper.pl
+++ root/plugins/video.watch.impress.co.jp/scraper.pl
@@ -3,7 +3,7 @@ sub init { }
 sub build_scraper {
     scraper {
         process ".backnoItem", "entries[]" => scraper {
-            process ".title_stapa", title => 'TEXT';
+            process '//div[contains(@class, "title_")]', title => 'TEXT';
             process ".subtitle", body => 'TEXT';
             process "img.top_mainimg", thumbnail => '@src';
             process ".button_play_flash a", link => '@href';
